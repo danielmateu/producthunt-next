e31e86c08bdfe4d0edffff5827785912cf893ba9
Daniel Mateu <danielmateu86@gmail.com>
2023-01-20 17:14:20 +0100

    Routing done

---
+++ src/pages/buscar.js
@@ -0,0 +1,13 @@
+import { Layout } from '@/components/layout/Layout'
+import React from 'react'
+
+const Buscar = () => {
+    return (
+        <>
+            <Layout />
+            <div>Buscar</div>
+        </>
+    )
+}
+
+export default Buscar
